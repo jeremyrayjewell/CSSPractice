383e82ecd54eba916e96b1ab8db59cf114962e78
jeremyrayjewell <jeremyrayjewell@gmail.com>
2023-10-25 22:59:09 -0300

    portfolio1

---
+++ portfolio1/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let codeElements = Array.from(document.getElementsByTagName('code'));
+    let textContents = codeElements.map(code => code.textContent);
+
+    setInterval(() => {
+        // Shuffle array
+        textContents.sort(() => Math.random() - 0.5);
+
+        // Assign new text contents to code elements
+        for (let i = 0; i < codeElements.length; i++) {
+            codeElements[i].textContent = textContents[i];
+        }
+    }, 1000);
+});
